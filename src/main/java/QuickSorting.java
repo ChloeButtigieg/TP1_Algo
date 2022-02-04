d4ddbdd99961f2c5c900a74154f8361a7ac11ba5
Chloe BUTTIGIEG <chloe.buttigieg12@gmail.com>
2022-02-04 17:01:25 +0100

    quick sorting

--- src/main/java/QuickSorting.java
+++ src/main/java/QuickSorting.java
@@ -8,14 +8,14 @@ public class QuickSorting {
         int firstEqualElementIndex = pivotIndex;
         int currentPosition = pivotIndex + 1;
         int firstGreaterElementIndex = maxIndex + 1;
-        while (currentPosition < firstEqualElementIndex) {
+        while (currentPosition < firstGreaterElementIndex) {
             if (array.get(currentPosition) < array.get(pivotIndex)) {
-                ArrayUtil.swap(array, array.get(currentPosition), array.get(firstEqualElementIndex));
+                ArrayUtil.swap(array, currentPosition, firstEqualElementIndex);
                 firstEqualElementIndex++;
                 pivotIndex++;
                 currentPosition++;
             } else if (array.get(currentPosition) > array.get(pivotIndex)) {
-                ArrayUtil.swap(array, array.get(currentPosition), array.get(firstGreaterElementIndex - 1));
+                ArrayUtil.swap(array, currentPosition, firstGreaterElementIndex - 1);
                 firstGreaterElementIndex--;
             } else {
                 currentPosition++;
@@ -25,12 +25,11 @@ public class QuickSorting {
         return pivotIndex;
     }
 
-    ArrayList<Integer> sorting(ArrayList<Integer> array, int minIndex, int maxIndex) {
+    void sorting(ArrayList<Integer> array, int minIndex, int maxIndex) {
         if (minIndex < maxIndex) {
-            int pivotIndex = flagDivision(array, minIndex, maxIndex, 0);
+            int pivotIndex = flagDivision(array, minIndex, maxIndex, minIndex);
             sorting(array, minIndex, pivotIndex - 1);
             sorting(array, pivotIndex + 1, maxIndex);
         }
-        return array;
     }
 }
